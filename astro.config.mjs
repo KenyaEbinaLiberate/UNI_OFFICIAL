a32ea78975678905007e66e9ebb1b1f0c0b12c17
KenyaEbinaLiberate <138367259+KenyaEbinaLiberate@users.noreply.github.com>
2025-04-01 14:21:38 +0900

    no message

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,4 +6,27 @@ export default defineConfig({
   devToolbar: {
     enabled: false,
   },
+  vite: {
+    assetsInclude: ["**/*.mp3", "**/*.svg", "**/*.jpg", "**/*.png"],
+    build: {
+      assetsInlineLimit: 0,
+      rollupOptions: {
+        output: {
+          assetFileNames: (assetInfo) => {
+            const type = assetInfo.name.split(".").pop();
+            if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(type)) {
+              return `images/[name][extname]`;
+            }
+            if (/mp3|wav|ogg/i.test(type)) {
+              return `audio/[name][extname]`;
+            }
+            return `assets/[name][extname]`;
+          },
+        },
+      },
+    },
+  },
+  build: {
+    assets: "assets",
+  },
 });
